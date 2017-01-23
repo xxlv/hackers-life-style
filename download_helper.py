5bb102475ee740f26497130afdf0aa077a242710
x <lvxx@dxyer.com>
2017-01-23 11:40:29 +0800

    Fix scripts

--- download_helper.py
+++ download_helper.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 from urllib.request import urlopen
-
 import hashlib
+import os
+from os.path import expanduser
+from shutil import copyfile
 
-DOWNLOAD_CACHE_DIR='~/Download/.download_helper'
+HOME = expanduser("~")
+DOWNLOAD_CACHE_DIR='{}/Downloads/downloadHelper'.format(HOME)
+BLOCK_SIZE=1024
 
 """
     简单下载工具
@@ -12,85 +16,102 @@ DOWNLOAD_CACHE_DIR='~/Download/.download_helper'
 """
 
 def download(url,path='.'):
-
-
-    _is_valid_url(url)
-
-    file_hash=hashlib.md5(url.encode('utf-8')).hexdigest()
-
-    print("File hash is {}".format(file_hash))
-
-
     _download_file(url,path)
-    # download from url
-    data=urlopen(url)
-
-
-
 
 
 def _download_file(url,target):
 
+    if not _is_valid_url(url):
+        exit(-1)
+
     file_name = url.split('/')[-1]
     u = urlopen(url)
     meta = u.info()
-    file_size = int(meta.get_all('Content-Length')[0])
+
+    if(meta.get_all('Content-Length')):
+        file_size = int(meta.get_all('Content-Length')[0])
+    else:
+        file_size=0
+
+    if file_size <=0:
+        exit(-1)
+
     file_hash=hashlib.md5(url.encode('utf-8')).hexdigest()
-    cache_target="{}/{}/{}".format(DOWNLOAD_CACHE_DIR,file_hash,target)
+    cache_target="{}/{}".format(DOWNLOAD_CACHE_DIR,file_hash)
     cache_target_INFO="{}/{}/{}".format(DOWNLOAD_CACHE_DIR,file_hash,"INFO")
+    cache_target_FILE="{}/{}".format(cache_target,"{}.data".format(file_hash))
+    _init_cache_INFO_ENV(cache_target,cache_target_INFO)
 
-    file_size_dl = _load_cache_INFO(cache_target_INFO)
+    file_size_dl = int(_load_cache_INFO(cache_target_INFO))
 
-    if file_size_dl == file_size:
-        print("Load from cache")
-        _copyfile(cache_target,target)
-        exit(0)
+    if not file_size_dl == file_size:
+        block_sz = BLOCK_SIZE
+        f = open(cache_target_FILE, 'wb')
 
-    block_sz = 8192
+        while True:
+            buffer =u.read(block_sz)
+            if not buffer:
+                break
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            _update_cache_INFO(cache_target_INFO,file_size_dl)
+            _print_process(file_size_dl,file_size)
+        f.close()
 
-    f = open(cache_target, 'wb')
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        _update_cache_INFO(file_size_dl)
-        print(status)
-
-    f.close()
-    _copyfile(cache_target,target)
+    _copyfile(cache_target_FILE,target)
 
 def _copyfile(src,target):
-    return sutill.copefile(src,target)
+    # TODO fast cp here 
+    if os.path.isfile(target):
+        print("File {} exists ".format(target))
+        exit(-1)
+
+    return copyfile(src,target)
+
+def _print_process(file_size_dl,file_size):
+
+    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status + chr(8)*(len(status)+1)
+    print(status)
 
 
+def _init_cache_INFO_ENV(cache_target,cache_target_INFO):
+
+    if not os.path.exists(cache_target):
+        os.makedirs(cache_target)
+        print("Create dir {}".format(cache_target))
+
+    if not os.path.isfile(cache_target_INFO):
+        open(cache_target_INFO,'w+').close()
+        print("Create file {}".format(cache_target_INFO))
+
+
+    return True
 
 def _update_cache_INFO(cache_target_INFO,offset):
-
-    with open(cache_target,'w+') as f:
-        f.write(offset)
+    with open(cache_target_INFO,'w+') as f:
+        f.write(str(offset))
 
 def _load_cache_INFO(cache_target_INFO):
+    with open(cache_target_INFO,'r') as f:
+        offset=f.read()
+    if not offset:
+        offset=0
+    else:
+        offset=int(offset)
 
-    with open(cache_target,'r') as f:
-        INFO=f.read()
-    return INFO
-
-
+    return offset
 
 def _is_local_url(url):
-    return
+    # TODO
+    return True
 
 def _is_valid_url(url):
-
     # TODO
     return True
 
 
+# MAIN
 if __name__=='__main__':
 
-    download('http://www.baidu.com','./http.d')
+    download('https://github.com/xxlv/hackers-life-style/archive/master.zip','master.zip')
